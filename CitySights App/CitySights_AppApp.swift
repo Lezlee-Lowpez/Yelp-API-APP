d7daf1ef5ff7d103390aae4bf62a91e7f4fbe385
Lesley Lopez <lopez.lesley96@yahoo.com>
2024-08-08 11:24:53 -0700

    Initial Commit

---
+++ CitySights App/CitySights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySights_AppApp.swift
+//  CitySights App
+//
+//  Created by Lesley Lopez on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CitySights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
