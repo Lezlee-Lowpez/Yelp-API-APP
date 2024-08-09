e433174a8ea3a1c2f807f86875e3cf02541049bc
Lesley Lopez <lopez.lesley96@yahoo.com>
2024-08-09 15:07:44 -0700

    - not sure why this is a separete commit? - this has random documentation in it and the helper functions

--- CitySights App/ContentView.swift
+++ CitySights App/ContentView.swift
@@ -15,8 +15,7 @@ struct ContentView: View {
     var body: some View {
         
         VStack{
-            
-            
+
             HStack {
                 TextField("What are you looking for?",text: $query)
                     .textFieldStyle(.roundedBorder)
@@ -29,13 +28,34 @@ struct ContentView: View {
                         .padding(.vertical, 10)
                 }
             }
-            List(businesses) {b in
-                Text(b.name ?? "nil")
+            List {
+                ForEach(businesses){b in
+                    VStack(spacing:20) {
+                        HStack(spacing:0) {
+                            Image("list-placeholder-image")
+                                .padding(.trailing, 16)
+                            VStack(alignment: .leading) {
+                                Text(b.name ?? "restaurant")
+                                    .font(Font.system(size: 15))
+                                    .bold()
+                                Text(TextHelper.distanceAwayText(meters: b.distance ?? 0))
+                                    .font(Font.system(size: 16))
+                                    .foregroundStyle(Color(red: 67/255, green: 71/255, blue: 76/255))
+                            }
+                            Spacer()
+                            Image("regular_\(TextHelper.roundTheRating(value: b.rating ?? 0))")
+                        }
+                        
+                        Divider()
+                    }
+                    
+                    
+                }
+                .listRowSeparator(.hidden)
             }
+            .listStyle(.plain)
         }
-        
-        
-        .padding()
+   
         .task {
              businesses = await service.businessSearch()
         }
--- CitySights App/Helpers/TextHelper.swift
+++ CitySights App/Helpers/TextHelper.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct TextHelper {
+    
+    static func distanceAwayText(meters: Double) -> String {
+        // static funcs allow you to call the method without having to create an instance of the struct seperately e.g you just call TextHelper.distanceAwayText
+        // instead of var helper = TextHelper()
+        // then helper.distanceAwayText
+        
+        if meters > 1000 {
+            return "\(Int(round(meters/1000))) km away"
+        } else {
+            return "\(Int(round(meters))) m away"
+        }
+    }
+    
+    
+    static func roundTheRating(value: Double) ->Double {
+        return (value * 2).rounded(.down) / 2.0
+    }
+    
+    
+}
