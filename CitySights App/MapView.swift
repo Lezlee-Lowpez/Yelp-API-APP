1999686dc1f30d21c72410e47dabd05504ef6529
Lesley Lopez <lopez.lesley96@yahoo.com>
2024-08-26 14:00:33 -0700

    -  add map markers

--- CitySights App/MapView.swift
+++ CitySights App/MapView.swift
@@ -9,8 +9,28 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    
+    @Environment(BusinessModel.self) var model
+    @State var selectedBusinessId: String?
+    
     var body: some View {
-        Map()
+        Map(selection: $selectedBusinessId){
+            ForEach(model.businesses, id: \.id) { b in
+                Marker(b.name ?? "restaurant", coordinate: CLLocationCoordinate2D(latitude: b.coordinates?.latitude ?? 0, longitude: b.coordinates?.longitude ?? 0))
+                .tag(b.id ?? "none")
+            }
+         
+        }
+        .onChange(of: selectedBusinessId) { oldValue, newValue in
+            // find the business that matches the id
+            let business = model.businesses.first { business in
+                business.id == selectedBusinessId
+            }
+            // if the business is found, set it as the selected one
+            if let business = business {
+                model.selectedBusiness = business
+            }
+        }
     }
 }
 
